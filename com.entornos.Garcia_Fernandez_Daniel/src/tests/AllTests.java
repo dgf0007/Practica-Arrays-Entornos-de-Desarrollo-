4946c863329048c15f9bb51f99f489f2f060b0cd
dgf0007 <dgf0007@alu.medac.es>
2023-04-17 17:35:42 +0200

    imports necesarios para Alltests

--- com.entornos.Garcia_Fernandez_Daniel/src/tests/AllTests.java
+++ com.entornos.Garcia_Fernandez_Daniel/src/tests/AllTests.java
@@ -3,6 +3,7 @@ package tests;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
+
 @Suite
 @SelectClasses({ test_Case.class })
 public class AllTests {
