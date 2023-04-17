4ed3202f930f0cb1411c45651ad21b0b214ee8e8
dgf0007 <dgf0007@alu.medac.es>
2023-04-17 17:29:21 +0200

    El cáculo de la mediana

--- com.entornos.Garcia_Fernandez_Daniel/src/tests/test_Case.java
+++ com.entornos.Garcia_Fernandez_Daniel/src/tests/test_Case.java
@@ -4,11 +4,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import com.entornos.Garcia_Fernandez_Daniel.Arrays;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+
 class test_Case {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	int array1[], array2[], array3[];
+
+	@BeforeAll
+	void iniciar() {
+		array1 = new int[] { 3, 7, 8, 4, 9, 10 };
+		array2 = new int[] { 6, 8, 5, 4, 9, 4, 12 };
+		array3 = new int[] { 5, 3, 7, 9, 12, 7, 1, 9, 10 };
 	}
+
+	@Test
+	void testMedia() {
+		assertEquals(7.0, Math.round(Arrays.media(array1)));
+		assertEquals(7.0, Math.round(Arrays.media(array2)));
+		assertEquals(7.0, Math.round(Arrays.media(array3)));
+	}
+
 
 }
