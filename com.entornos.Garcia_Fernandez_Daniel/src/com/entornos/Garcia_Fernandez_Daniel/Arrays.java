fe3a8938f82d01e841d845bc59285af20e7ab5f2
dgf0007 <dgf0007@alu.medac.es>
2023-04-17 17:59:59 +0200

    PDF terminado

--- com.entornos.Garcia_Fernandez_Daniel/src/com/entornos/Garcia_Fernandez_Daniel/Arrays.java
+++ com.entornos.Garcia_Fernandez_Daniel/src/com/entornos/Garcia_Fernandez_Daniel/Arrays.java
@@ -2,19 +2,7 @@ package com.entornos.Garcia_Fernandez_Daniel;
 
 public abstract class Arrays {
 
-	/**
-	 * Método para calcular la media de un array de números enteros
-	 * Recibe un array de números enteros
-	 * 
-	 * @param array
-	 * 
-	 * @version 1.0
-	 * 
-	 * @author daniel
-	 * 
-	 * @return devuelve la media con decimales
-	 * 
-	 **/
+
 	public static double media(int[] array) {
 		int sumatorio = 0;
 		for (int i = 0; i < array.length; i++) {
@@ -23,19 +11,7 @@ public abstract class Arrays {
 		return (double) sumatorio / array.length;
 	}
 
-	/**
-	 * Método para calcular la mediana de un array de números enteros
-	 * Recibe un array de números enteros
-	 * 
-	 * @param array
-	 * 
-	 * @version 1.0
-	 * 
-	 * @author daniel
-	 * 
-	 * @return devuelve la mediana con decimales
-	 * 
-	 **/
+
 	public static double mediana(int[] array) {
 		Arrays.sort(array);
 		int middle = array.length / 2;
@@ -51,19 +27,7 @@ public abstract class Arrays {
 
 	}
 
-	/**
-	 * Método para calcular el máximo valor de un array de números enteros
-	 * Recibe un array de números enteros
-	 * 
-	 * @param array
-	 * 
-	 * @version 1.0
-	 * 
-	 * @author daniel
-	 * 
-	 * @return devuelve el máximo valor disponible en el array
-	 * 
-	 **/
+
 	public static int maximo(int[] array) {
 		int maximo = Integer.MIN_VALUE;
 		for (int i = 0; i < array.length; i++) {
@@ -74,19 +38,6 @@ public abstract class Arrays {
 		return maximo;
 	}
 
-	/**
-	 * Método para calcular el mínimo valor de un array de números enteros
-	 * Recibe un array de números enteros
-	 * 
-	 * @param array
-	 * 
-	 * @version 1.0
-	 * 
-	 * @author daniel
-	 * 
-	 * @return devuelve el mínimo valor disponible en el array
-	 * 
-	 **/
 	public static int minimo(int[] array) {
 		int minimo = Integer.MAX_VALUE;
 		for (int i = 0; i < array.length; i++) {
